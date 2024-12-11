5687214fad3dbc06259475442ba76cbd29f8ba3e
Mistahhh <118472546+Mistahhh@users.noreply.github.com>
2024-12-11 21:47:15 +0000

    some changes

--- classwork.cpp
+++ classwork.cpp
@@ -5,8 +5,11 @@ int main()
 {
   int age;
   age = 10;
-  int numberofpets;
+  bool hasPets = true;
   int grade = 11;
   double price = 19.99;
+
+  std::cout << age << " " << price << " " << hasPets;
+
   return 0;
 }
